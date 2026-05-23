2ccd48f5634e1886d2d9f2a9d46a70400163df89
TOMOHIRO KUDO <t.kudo.1060.pc@gmail.com>
2026-05-23 19:00:16 +0900

    Change CORS settings to allow all origins

--- backend/app/main.py
+++ backend/app/main.py
@@ -28,10 +28,7 @@ app = FastAPI(
 # CORS設定 (フロントエンドからのアクセスを許可)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000",
-        os.getenv("FRONTEND_URL", ""),
-    ],
+　　 allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
