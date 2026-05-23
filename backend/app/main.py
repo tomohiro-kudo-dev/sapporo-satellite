e2e85e1e57f4ae90f7ca4d70567f2bda28207efa
TOMOHIRO KUDO <t.kudo.1060.pc@gmail.com>
2026-05-23 19:04:38 +0900

    Fix formatting of allow_origins in CORS settings

--- backend/app/main.py
+++ backend/app/main.py
@@ -28,7 +28,7 @@ app = FastAPI(
 # CORS設定 (フロントエンドからのアクセスを許可)
 app.add_middleware(
     CORSMiddleware,
-　　 allow_origins=["*"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
